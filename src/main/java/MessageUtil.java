9b50a0a2b6644a66369ea53e2157389a43f1e81e
dong.chen <dong.chen@hbo.com>
2017-05-16 02:17:10 -0400

    Add dev branch

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
   }
 
   public String salutationMessage() {
-    message = "Hi!" + message;
+    message = "Hi Dev!" + message;
     System.out.println(message);
     return message;
   }
