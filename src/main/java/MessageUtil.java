4545438542ae2f86567f709459948e7c349844ed
dong.chen <dong.chen@hbo.com>
2017-05-16 02:26:17 -0400

    Edit MessageUtil.java

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
   }
 
   public String salutationMessage() {
-    message = "Hi Dev1!" + message;
+    message = "Hi Dev!" + message;
     System.out.println(message);
     return message;
   }
